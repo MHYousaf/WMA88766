56ff3a417212355065e3aece83865ff63eee8767
MHYousaf <101709883+MHYousaf@users.noreply.github.com>
2023-04-06 20:34:42 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,181 +1,127 @@
-document.getElementById("main").style.display = "none";
+// Get input value function
 
-validation = () => {
-    // Full Name validation
-    let text = document.getElementById("fname").value;
-    if (text == "" || text == null) {
-        Toastify({
-            text: "Enter Your Full Name Plz!",
-            duration: 2000,
-            destination: "https://github.com/apvarun/toastify-js",
-            newWindow: true,
-            close: true,
-            gravity: "top", // `top` or `bottom`
-            position: "center", // `left`, `center` or `right`
-            stopOnFocus: true, // Prevents dismissing of toast on hover
-            style: {
-              background: "linear-gradient(to right, #00b09b, #96c93d)",
-            },
-            onClick: function(){} // Callback after click
-          }).showToast();
-        return
-    }
-    text = text.toLowerCase();
-    let first = text.slice(0, 1);
-    first = first.toUpperCase();
-    let allOthers = text.slice(1);
-    allOthers = allOthers.toLowerCase();
-    let  fullName = first + allOthers;
+let getValFunction = (id)=>{
+    return document.getElementById(id).value;
+}
 
-    // email validation
-    let email = document.getElementById("myE").value;
-    if (email == "" || email == null) {
-        Toastify({
-            text: "Enter Email Plz!",
-            duration: 2000,
-            destination: "https://github.com/apvarun/toastify-js",
-            newWindow: true,
-            close: true,
-            gravity: "top", // `top` or `bottom`
-            position: "center", // `left`, `center` or `right`
-            stopOnFocus: true, // Prevents dismissing of toast on hover
-            style: {
-              background: "linear-gradient(to right, #00b09b, #96c93d)",
-            },
-            onClick: function(){} // Callback after click
-          }).showToast();
-        return
-    }else if(email != "admin@user.com"){
-        Toastify({
-            text: "Enter Correct Email Plz!",
-            duration: 2000,
-            destination: "https://github.com/apvarun/toastify-js",
-            newWindow: true,
-            close: true,
-            gravity: "top", // `top` or `bottom`
-            position: "center", // `left`, `center` or `right`
-            stopOnFocus: true, // Prevents dismissing of toast on hover
-            style: {
-              background: "linear-gradient(to right, #00b09b, #96c93d)",
-            },
-            onClick: function(){} // Callback after click
-          }).showToast();
-    }
+// Notify function
+let notify = (msg,color)=>{
+    Toastify({
+        text: msg,
+        duration: 2000,
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: false, // Prevents dismissing of toast on hover
+        style: {
+          background:color,
+          color:"black",
+          borderRadius:'10px'
+        },
+        onClick: function(){} // Callback after click
+      }).showToast();
+}
 
-    // password validation
-    let pass = document.getElementById("myPass").value;
-    if(pass == "" || pass == null){
-        Toastify({
-            text: "Enter Password Plz!",
-            duration: 2000,
-            destination: "https://github.com/apvarun/toastify-js",
-            newWindow: true,
-            close: true,
-            gravity: "top", // `top` or `bottom`
-            position: "center", // `left`, `center` or `right`
-            stopOnFocus: true, // Prevents dismissing of toast on hover
-            style: {
-              background: "linear-gradient(to right, #00b09b, #96c93d)",
-            },
-            onClick: function(){} // Callback after click
-          }).showToast();
+// Get age function
+let getAgeFunction = (dob)=>{
+    var birthDate = new Date(dob);
+    var diffenrance = Date.now() - birthDate.getTime();
+
+    var ageDate  = new Date(diffenrance);
+
+    var agecalculate = Math.abs(ageDate.getUTCFullYear() - 1970);
+
+    return agecalculate;
+}
+// Generate Id
+let generateId = ()=>{
+
+    let id = Math.random().toString(36).slice(2);
+    // console.log(id)
+    return id;
+}
+let users = [];
+// Show user table
+
+let showUserTable = ()=>{
+    if(users.length<1)
+    {
+        notify('Result not found!','#ffc107')
         return;
-
     }
-    else if(pass !="123456" ){
-        Toastify({
-            text: "Enter correct Password Plz!",
-            duration: 2000,
-            destination: "https://github.com/apvarun/toastify-js",
-            newWindow: true,
-            close: true,
-            gravity: "top", // `top` or `bottom`
-            position: "center", // `left`, `center` or `right`
-            stopOnFocus: true, // Prevents dismissing of toast on hover
-            style: {
-              background: "linear-gradient(to right, #00b09b, #96c93d)",
-            },
-            onClick: function(){} // Callback after click
-          }).showToast();
-          return;
+
+    let tbody = "";
+    let sr = 1;
+
+    for(user of users)
+    {
+        tbody+=`
+        <tr>
+            <td>${sr++}</td>
+            <td>${user.firstname}</td>
+            <td>${user.lastname}</td>
+            <td>${user.email}</td>
+            <td>${user.age}</td>
+        </tr>
+        `;
+    }
+
+    document.getElementById('tbody').innerHTML = tbody;
+}
+// Add user function
+let addUserFunction = ()=>{
+    event.preventDefault();
+    let firstname = getValFunction('firstname');
+    let lastname = getValFunction('lastname');
+    let email = getValFunction('email');
+    let dob = getValFunction('dob');
+
+    if(firstname.length < 3)
+    {
+       notify('Please enter vaild firstname','#ffc107')
+       return;
+    }
+    if(lastname.length < 3)
+    {
+       notify('Please enter vaild lastname','#ffc107')
+       return;
+    }
+
+    let vaildEmail =  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if(!email.match(vaildEmail)){
+        notify('Please enter vaild email!','#ffc107')
+        return;
+    }
+    if(!dob){
+        notify('Please enter date of birth','#ffc107')
+        return;
     }
     
-    document.getElementById("sign").style.display = "none";
-    document.getElementById("homeName").innerHTML = fullName;
-    document.getElementById("main").style.display = "block";
-    Toastify({
-      text: "Welcome "+fullName +"!",
-      duration: 2000,
-      destination: "https://github.com/apvarun/toastify-js",
-      newWindow: true,
-      close: true,
-      gravity: "top", // `top` or `bottom`
-      position: "center", // `left`, `center` or `right`
-      stopOnFocus: true, // Prevents dismissing of toast on hover
-      style: {
-        background: "linear-gradient(to right, #00b09b, #96c93d)",
-      },
-      onClick: function(){} // Callback after click
-    }).showToast();
+    let user = {
+        id:generateId(),
+        firstname,
+        lastname,
+        email,
+        dob,
+        age:getAgeFunction(dob),
+    }
+    users.push(user);
+    notify('Add user successfully!','#14A44D')
+    showUserTable();
 }
 
-let cities = ["Islamabad", "Lahore", "Karachi", "Peshawar", "Quetta"]
-
-for (i = 0; i < cities.length; i++) {
-  num = i + 1;
-  document.getElementById("original-string").innerHTML += num + ")" + " " + cities[i] + "<br>"
-}
-function printCities(){
-  document.getElementById("output").innerHTML = " ";
-
-  for (i = 0; i < cities.length; i++) {
-      let num = i + 1
-      document.getElementById("output").innerHTML += num + ")" + " " + cities[i] + " " + "<br>"
-  }
-}
-
-function addCity(){
-  let newCity = document.getElementById("input").value ;
-  if (!newCity) {
-    Toastify({
-      text: "Enter City Name Plz!",
-      duration: 2000,
-      destination: "https://github.com/apvarun/toastify-js",
-      newWindow: true,
-      close: true,
-      gravity: "top", // `top` or `bottom`
-      position: "center", // `left`, `center` or `right`
-      stopOnFocus: true, // Prevents dismissing of toast on hover
-      style: {
-        background: "linear-gradient(to right, #00b09b, #96c93d)",
-      },
-      onClick: function(){} // Callback after click
-    }).showToast();
-      return;
-  }
-  newCityFirstLetter = newCity.charAt(0).toUpperCase();
-  newCityAllLetters = newCity.slice(1).toLowerCase();
-  newCityInCapitalize = newCityFirstLetter + newCityAllLetters;
-
-  let cityFound = false;
-  for (i = 0; i < cities.length; i++) {
-      if (cities[i] === newCityInCapitalize) {
-          cityFound = true;
-          document.getElementById("output").innerHTML = "<span style='color:red'>" + "&quot" + newCityInCapitalize + "&quot" + '</span>' + " is already in cities list"
-      }
-  }
-
-  if (cityFound === false) {
-      cities.push(newCityInCapitalize);
-      document.getElementById("output").innerHTML = "<span style='color:green'>" + newCityInCapitalize + "&quot" + '</span>' + " has been successfully added into the list"
-
-      document.getElementById("print-cities").onclick = function () {
-          document.getElementById("output").innerHTML = " "
-          for (i = 0; i < cities.length; i++) {
-              num = i + 1;
-              document.getElementById("output").innerHTML += num + ")" + " " + cities[i] + "<br>"
-          }
-      }
-
-  }
+// show user in console
+let showUserInConsole = ()=>{
+    console.log('');
+    if(users.length<1)
+    {
+        notify('Result not found!','#ffc107')
+        return;
+    }
+    for(user of users)
+    {
+        console.log(user)
+    }
+    
 }
